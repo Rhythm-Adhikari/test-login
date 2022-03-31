b76b673395bc1f3c35f40d477f0b56e6e36b0b76
rhythm <rhythm12_adhikari@hotmail.com>
2022-04-01 10:58:44 +1100

    test

---
+++ src/addproduct.js
@@ -0,0 +1,9 @@
+function addproduct()
+{
+    return(
+        <div>
+            <h1>add product page</h1>
+        </div>
+    )
+}
+export default addproduct()
---
+++ src/updateproduct.js
@@ -0,0 +1,9 @@
+function updateproduct()
+{
+    return(
+        <div>
+            <h1>update product page</h1>
+        </div>
+    )
+}
+export default updateproduct()
